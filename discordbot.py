a0010080511359608cb0d101886662badf879578
Haoming <nh.haoming@gmail.com>
2021-05-03 21:31:52 +0900

    作業途中

--- discordbot.py
+++ discordbot.py
@@ -5,51 +5,113 @@ import discord
 from flask import Flask, render_template, request, redirect, url_for
 import os
 
+Intents = discord.Intents.default()
+Intents.members = True
+
 class DiscordBot:
     __instance = None
     TOKEN = ""
-    client = discord.Client()
+    client = discord.Client(intents=Intents)
+    dcUsers = []
+    auUsers = {} # キー = ゲーム内名、バリュー = Discrodユーザー
+    vc = None
+    msg = None
 
     def getInstance():
         if DiscordBot.__instance == None:
             DiscordBot()
         return DiscordBot.__instance
 
-    def __init__(self):
-        if DiscordBot.__instance != None:
-            raise Exception("This class is a singleton!")
-        else:
-            self.server = Process(target=self.runFlaskProcess)
-            DiscordBot.__instance = self
-
     @client.event
     async def on_ready():
         # 起動したらターミナルにログイン通知が表示される
         print('ログインしました')
+
     @client.event
     async def on_message(message):
         content = message.content
         webserver = WebServer.getInstance()
+        discordbot = DiscordBot.getInstance()
         if re.match('^/msm [a-z]*$', content):
             command = content.split(" ")[1]
             print(command)
             print(content)
             if command == "new" or command == "n":
-                # プラグインからゲームステータスを受け取るサーバーを起動する
-                if webserver.isRunning:
-                    webserver.stopFalsk()
-                webserver.startFlask()
+                await discordbot.new(message)
             elif command == "end" or command == "e":
-                # プラグインからゲームステータスを受け取るサーバーを終了する
-                if webserver.isRunning:
-                    webserver.stopFlask()
+                await discordbot.end()
             elif command == "unmute" or command == "u":
-                # チャンネル内の全員のミュートステータスを解除する
-                pass
+                await discordbot.unmute()
+            elif command == "test":
+                await discordbot.test(message)
+            elif command == "testa":
+                await discordbot.test2(message)
             else:
-                # コマンドのhelpを投稿
-                pass
+                await discordbot.help()
+
             # コマンドを実行後に元のメッセージを削除する
+            await message.delete()
+            print("delete OK")
+
+    async def new(self,message):
+        # サーバー起動のメッセージを作成する
+        # メッセージ送り主のいるボイスチャンネルを探す
+        author = message.author
+        vcs = message.guild.voice_channels
+        targetvc = None
+        for vc in vcs:
+            if targetvc != None:
+                break
+            for member in vc.members:
+                if targetvc !=None:
+                    break
+                if member == author:
+                    targetvc = vc
+                    break
+        print(tagetvc)
+        self.vc = targetvc
+        # プラグインからゲームステータスを受け取るサーバーを起動する
+        webserver = WebServer.getInstance()
+        if webserver.isRunning:
+            webserver.stopFalsk()
+        webserver.startFlask()
+
+    async def end(self):
+        # サーバー起動のメッセージを削除する
+        # プラグインからゲームステータスを受け取るサーバーを終了する
+        webserver = WebServer.getInstance()
+        if webserver.isRunning:
+            webserver.stopFlask()
+
+    async def unmute(self):
+        # チャンネル内の全員のミュートステータスを解除する
+        pass
+
+    async def help(self):
+        pass
+
+    async def test2(self,message):
+        if self.msg:
+            print(self.msg)
+            await self.msg.delete()
+
+    async def test(self,message):
+        # サーバー起動のメッセージを作成
+        print(message.channel)
+        self.msg = await message.channel.send("MuteBot開始\nクライアント待ち")
+        # メッセージ送り主のいるボイスチャンネルを探す
+        vcs = message.guild.voice_channels
+        author = message.author
+        targetvc = None
+        for vc in vcs:
+            if targetvc != None:
+                break
+            for member in vc.members:
+                if targetvc !=None:
+                    break
+                if member == author:
+                    targetvc = vc
+                    break
 
     # ゲーム開始時、ミーティング終了時に実行
     def startTasks():
@@ -71,8 +133,8 @@ class DiscordBot:
             raise Exception("This class is a singleton!")
         else:
             self.TOKEN = TOKEN
-            self.client.run(self.TOKEN)
             DiscordBot.__instance = self
+            self.client.run(self.TOKEN)
         
 
 
